016bddbdcb8e525c484e700d77f5dba8a13f65c5
Ken Tabor <kenneth.tabor@sabre-holdings.com>
2012-10-25 10:29:38 -0500

    breaking points highlighted, code cleanup

--- Responsizer.js
+++ Responsizer.js
@@ -1,21 +1,46 @@
 (function() {
 
+	var breakPoints = [320, 480, 768, 1024, 1600];
+	var isBreakPointShown = false;
 	var elStatus = $('<span>W x H</span>');
 
-	elStatus.css('background-color', 'black');
-	elStatus.css('color', 'white');
-	elStatus.css('padding', '5px');
-	elStatus.css('z-index', '50');
-	elStatus.css('position', 'fixed');
-	elStatus.css('left', '50%');
-	elStatus.css('width', '75px');
-	elStatus.css('margin-left', '-37px');
-	elStatus.css('text-align', 'center');
+	StatusInit();
 
-	$('body').append(elStatus);
+	function StatusInit() {
+		elStatus.css('background-color', 'black');
+		elStatus.css('color', 'white');
+		elStatus.css('left', '50%');
+		elStatus.css('margin-left', '-45px');
+		elStatus.css('padding', '5px');
+		elStatus.css('position', 'fixed');
+		elStatus.css('text-align', 'center');
+		elStatus.css('top', '0');
+		elStatus.css('width', '90px');
+		elStatus.css('z-index', '250');
 
-	$(window).resize(function() {
-		elStatus.html(window.innerWidth + ' x ' + window.innerHeight);
-	});
+		$('body').append(elStatus);
+		StatusUpdate();
+
+		$(window).resize(function() {
+			StatusUpdate();
+		});
+	}
+
+	function StatusUpdate() {
+		var curWidth = window.innerWidth;
+		var curHeight = window.innerHeight;
+
+		elStatus.html(curWidth + ' x ' + curHeight);
+
+		if (breakPoints.indexOf(curWidth) !== -1) {
+			elStatus.css('color', 'yellow');
+			elStatus.css('background-color', 'slateGray');
+			isBreakPointShown = true;
+		} else if (isBreakPointShown) {
+			elStatus.css('color', 'white');
+			elStatus.css('background-color', 'black');
+			isBreakPointShown = false;
+		}
+	}
 
 }) ();
